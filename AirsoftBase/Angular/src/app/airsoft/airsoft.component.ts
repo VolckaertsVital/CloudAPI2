f5e1fc7978893c58d3c75963e406a9ccc2306a97
Vital <vitalv@skynet.be>
2020-04-07 18:16:33 +0200

    nav bar toegevoegt airsoftcomponent verder uitgewerkt

--- AirsoftBase/Angular/src/app/airsoft/airsoft.component.ts
+++ AirsoftBase/Angular/src/app/airsoft/airsoft.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+
 
 @Component({
   selector: 'app-airsoft',
   templateUrl: './airsoft.component.html',
   styleUrls: ['./airsoft.component.scss']
 })
-export class AirsoftComponent implements OnInit {
+export class AirsoftComponent{
 
-  constructor() { }
+  public Airsoft: airsofts[];
 
-  ngOnInit() {
+  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
+    http.get<airsofts[]>(baseUrl + 'airsofts').subscribe(result => {
+      this.Airsoft = result;
+    }, error => console.error(error));
   }
+}
 
+interface airsofts {
+  Brand: string;
+  Type: string;
+  Name: string;
+  MagazineSize: number;
+  FPS: number;
+  Caliber: number;
+  Joule: number;
+  GearBox: string;
+  Lenght: number;
+  Weight: number;
+  Battery: boolean;
+  Description: string;
 }
--- AirsoftBase/Angular/src/app/app.module.ts
+++ AirsoftBase/Angular/src/app/app.module.ts
@@ -5,18 +5,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AirsoftComponent } from './airsoft/airsoft.component';
 import { RouterModule } from '@angular/router';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AirsoftComponent
+    AirsoftComponent,
+    NavMenuComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     RouterModule.forRoot([
-      { path: 'airsoft', component: AirsoftComponent }
-    ])
+      { path: "airsoft", component: AirsoftComponent },
+      { path: "", redirectTo: "airsoft", pathMatch: "full" }
+    ], { useHash: true })
   ],
   providers: [],
   bootstrap: [AppComponent]
